ec5c223c949c9aaebdd0172de45252d135315ec3
Ben Barnett <ben@univthink.com>
2018-04-09 17:20:38 -0400

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,7 @@
 export const API_URL = process.env.NODE_ENV === 'production' ?
                       'https://rubyrubyrails.herokuapp.com/api' :
-                      'http://localhost:5000/api';
+                      'https://rubyrubyrails.herokuapp.com/api';
 
 export const WS_URL =  process.env.NODE_ENV === 'production' ?
                        'wss://rubyrubyrails.herokuapp.com/api/cable' :
-                       'ws://localhost:5000/api/cable';
+                       'wss://rubyrubyrails.herokuapp.com/api/cable';
